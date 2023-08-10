491f9dfc4b09ce82215ed63ffa6d6bb738f31837
rohit9123 <60548779+rohit9123@users.noreply.github.com>
2023-08-10 08:36:00 +0530

    removed conflict code

--- app.vue
+++ app.vue
@@ -1,13 +1,5 @@
 <template>
-<<<<<<< HEAD
   <div>
     <Calender />
   </div>
-=======
-  <main>
-    <div>
-      Hello
-    </div>
-  </main>
->>>>>>> 10145abc2720288155f17d9abfca1699618e2e9d
 </template>
